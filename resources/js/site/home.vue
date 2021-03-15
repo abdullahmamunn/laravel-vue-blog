507031b13087eba7db55ad31f15639de5db21963
abdullahmamunn <almamun00021@gmail.com>
2021-03-15 17:19:59 +0600

    home page changes

--- resources/js/site/home.vue
+++ resources/js/site/home.vue
@@ -9,7 +9,7 @@
             <img class="card-img-top" :src="imageUrl(post.thumbnail)" alt="Card image cap">
             <div class="card-body">
                 <h2 class="card-title">{{post.title}}</h2>
-                <p class="card-text">{{post.description}}</p>
+                <sometag v-html=post.description></sometag>
                 <a href="#" class="btn btn-primary">Read More &rarr;</a>
             </div>
             <div class="card-footer text-muted">
