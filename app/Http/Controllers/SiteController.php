e4646597ed8d404227b47ec40592cdef9e478818
abdullahmamunn <almamun00021@gmail.com>
2021-03-15 16:17:58 +0600

    category show in home page

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Category;
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
@@ -14,5 +15,10 @@ class SiteController extends Controller
                          ->orderBy('created_at','desc')
                          ->paginate(3);
         return response()->json($all_posts,200);
+    }
+    public function showCategories()
+    {
+        $show_categories = Category::where('status',1)->get();
+        return response()->json($show_categories,200);
     }
 }
--- resources/js/site/home.vue
+++ resources/js/site/home.vue
@@ -9,7 +9,7 @@
             <img class="card-img-top" :src="imageUrl(post.thumbnail)" alt="Card image cap">
             <div class="card-body">
                 <h2 class="card-title">{{post.title}}</h2>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                <p class="card-text">{{post.description}}</p>
                 <a href="#" class="btn btn-primary">Read More &rarr;</a>
             </div>
             <div class="card-footer text-muted">
@@ -48,14 +48,8 @@
                 <div class="row">
                     <div class="col-lg-6">
                         <ul class="list-unstyled mb-0">
-                            <li>
-                                <a href="#">Web Design</a>
-                            </li>
-                            <li>
-                                <a href="#">HTML</a>
-                            </li>
-                            <li>
-                                <a href="#">Freebies</a>
+                            <li v-for="category in categories">
+                                <a href="#">{{category.name}}</a>
                             </li>
                         </ul>
                     </div>
@@ -93,19 +87,19 @@
         name: "home",
         data(){
             return{
-                 allPosts:{}
+                 allPosts:{},
             }
         },
         mounted() {
-            // this.$store.dispatch('getAllPosts');
+            this.$store.dispatch('getActiveCategories');
             this.getResults()
         },
-        // computed:{
-        //     posts()
-        //     {
-        //         this.allPosts = this.$store.getters.getPosts
-        //     }
-        // },
+        computed:{
+            categories()
+            {
+               return this.$store.getters.getActiveCategories;
+            }
+        },
         methods:{
             getResults(page = 1) {
                 axios.get('/all-posts?page=' + page)
